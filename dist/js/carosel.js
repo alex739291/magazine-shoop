491a29e3fe8d3ab3cd91562dbb7e2b1255d27f44
Alex Savchuk <alexsavchuk739291@gmail.com>
2016-06-28 19:32:58 +0300

    last change

--- dist/js/carosel.js
+++ dist/js/carosel.js
@@ -37,3 +37,35 @@ $(document).ready(function() {
     });
 
 });
+
+
+
+$(document).ready(function() {
+
+    $("#owl-newslistcol").owlCarousel({
+
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+        items : 3,
+        itemsDesktopSmall: [1199,3],
+        itemsTablet: [992,2],
+        itemsTabletSmall : [768,1]
+
+    });
+
+});
+
+$(document).ready(function() {
+
+    $("#owl-brands").owlCarousel({
+
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+        items : 6,
+        itemsDesktopSmall: [1199,6],
+        itemsTablet: [992,4],
+        itemsTabletSmall : [768,2]
+
+    });
+
+});
---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+ymaps.ready(init);
+var myMap,
+    myPlacemark;
+
+function init(){
+    myMap = new ymaps.Map ("map", {
+        center: [55.76, 37.64],
+        zoom: 7
+    });
+
+    myPlacemark = new ymaps.Placemark([55.76, 37.64], { content: 'Москва!', balloonContent: 'Столица России' });
+}
